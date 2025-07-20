db45969de2b235dfa4ab70e165a528ebae2d999d
yulianakhabibovaa <yulianakhabibovaa@yandex.ru>
2025-07-21 02:36:33 +0300

    еще правки

--- ewm-main/src/main/java/ru/practicum/ewm/request/dto/EventRequestStatusUpdateResult.java
+++ ewm-main/src/main/java/ru/practicum/ewm/request/dto/EventRequestStatusUpdateResult.java
@@ -10,6 +10,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EventRequestStatusUpdateResult {
-    private List<ParticipationRequestDto> confirmedRequests;
-    private List<ParticipationRequestDto> rejectedRequests;
+    private List<ParticipationRequestDto> confirmedRequests = List.of();
+    private List<ParticipationRequestDto> rejectedRequests = List.of();
 }
