e47332185ad393e13810b03d79e91dd85a2dea0d
yulianakhabibovaa <yulianakhabibovaa@yandex.ru>
2025-07-21 05:34:49 +0300

    еще правка форматов

--- ewm-main/src/main/java/ru/practicum/ewm/request/model/ParticipationRequest.java
+++ ewm-main/src/main/java/ru/practicum/ewm/request/model/ParticipationRequest.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.request.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.user.model.User;
 
@@ -45,6 +47,8 @@ public class ParticipationRequest {
     @Column(nullable = false)
     private RequestStatus status;
 
-    @Column(nullable = false)
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    @Column(nullable = false, updatable = false)
     private LocalDateTime created;
 }
